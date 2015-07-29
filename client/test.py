7a34fc2f4fea9946d5cb4b6ba110b6c1689e7f2d
wayne163 <weiyunshen@gmail.com>
2015-07-29 22:25:06 +0800

    add Cipher

--- client/test.py
+++ client/test.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 __author__ = 'wayne'
+from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 import requests, json
 public_key_path = 'public_key.pem'
@@ -14,8 +15,9 @@ def getPublicKey():
 
 def encryptMessage(message):
     p_key = getPublicKey()
-    result = p_key.encrypt(token, nonce)
-    #print 'client encrypt message %s' % result
+    # result = p_key.encrypt(token, nonce)
+    cipher = PKCS1_OAEP.new(p_key)
+    result = cipher.encrypt(message)
     return result
 
 if __name__ == '__main__':
@@ -23,8 +25,9 @@ if __name__ == '__main__':
     safe_url = 'http://127.0.0.1:8000/safe_message/'
     session = requests.Session()
     message = encryptMessage(token)
+    print message.encode('string-escape')
     data = {
-        'message': str(message),
+        'message': message.encode('string-escape'),
     }
     r = session.post(safe_url, json.dumps(data))
     print r.text
